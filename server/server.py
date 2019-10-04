4429ab26f87740278bb350d4eede5f5eda508aac
Gustaf Sjoberg <gs@trell.se>
2019-10-04 15:09:10 +0200

    Adds commandline arguments.

--- server/server.py
+++ server/server.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
+import argparse
 import asyncio
 import websockets
 import serial
 
-arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
+parser = argparse.ArgumentParser(description="Armstrong serial-ws bridge")
+parser.add_argument('--serial', help='Serial device', type=str, default='/dev/ttyACM0')
+parser.add_argument('--port', help='WebSockets port', type=int, default=8181)
 
+args = parser.parse_args()
+
+arduino = serial.Serial(args.serial, 115200, timeout=.1)
 sockets = set()
 
 async def ws_connection_handler(socket, path):
     sockets.add(socket)
     while True:
         message = await socket.recv()
-        print(message)
         arduino.write(bytes(message, 'utf8'))
     sockets.remove(websocket)
 
 async def consume(con):
-    print(con)
     while True:
         msg = con.readline()
         if msg:
-            print(msg)
-        asyncio.gather(*[socket.send(msg.decode('utf8')) for socket in sockets])
-        await asyncio.sleep(2)
+            asyncio.gather(*[socket.send(msg.decode('utf8')) for socket in sockets])
+        await asyncio.sleep(0)
 
-loop = asyncio.get_event_loop()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    tasks = asyncio.gather(
+        websockets.serve(ws_connection_handler, '0.0.0.0', args.port),
+        consume(arduino),
+    )
 
-loop.run_until_complete(asyncio.gather(
-    websockets.serve(ws_connection_handler, '0.0.0.0', 8181),
-    consume(arduino),
-))
-
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-finally:
-    loop.close()
-    print("Bye!")
+    try:
+        loop.run_until_complete(tasks)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
